b14bf799518d1a7fcbf50accff4f76c15b5aa5de
n-rezinkin <89951085+n-rezinkin@users.noreply.github.com>
2021-11-06 05:56:51 +0300

    55
    5555

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ const priceItems = document.querySelectorAll(".price__right-item");
 
 const priceRightTabs = document.querySelectorAll(".price__right-tab");
 const priceRightItems = document.querySelectorAll(".price__right-content");
+// const goldBox = document.querySelectorAll(".price__right-tab-box");
+// const goldTab = goldBox.querySelector("button");
 
 if (priceTabs) {
    priceTabs.forEach(function (item) {
@@ -10,6 +12,10 @@ if (priceTabs) {
          let currentBtn = item;
          let tabId = currentBtn.getAttribute("data-tab");
          let currentTab = document.querySelector(tabId);
+         // let goldTab = currentTab.querySelector('button');
+         // let goldContent = currentTab.querySelector('.price__right-content');
+
+
 
          if (!currentBtn.classList.contains('active')) {
             priceTabs.forEach(function (item) {
@@ -18,10 +24,14 @@ if (priceTabs) {
 
             priceItems.forEach(function (item) {
                item.classList.remove('active');
-            })
+            });
+
 
             currentBtn.classList.add('active');
             currentTab.classList.add('active');
+            // goldTab.classList.add('active');
+            // goldContent.classList.add('active');
+
 
          }
       });
@@ -61,7 +71,11 @@ gradient.addColorStop(1, 'rgba(255, 255, 255, 0.01)');
 let goldData = [];
 for (var i = 0, l = 12; i < l; i++) {
    goldData.push(Math.round(Math.random() * (2400 - 2000) + 2000))
+   // console.log(goldData[11])
 };
+// document.querySelectorAll('.graphics__top-price')[0].firstChild.innerHTML = goldData[11];
+// console.log(document.querySelectorAll('.graphics__top-price')[0].firstChild.innerHTML)
+
 
 let gold585Data = {
    labels: ["01.01", "01.02", "01.03", "01.04", "01.05", "01.06", "01.07", "01.08", "01.09", "01.10", "01.11", "01.12"],
@@ -82,6 +96,10 @@ let chartOptions = {
       legend: {
          display: false,
       },
+   },
+
+   animation: {
+      duration: 3000
    },
 
    scales: {
@@ -108,7 +126,6 @@ let lineChart = new Chart(gold585Canvas, {
    type: 'line',
    data: gold585Data,
    options: chartOptions
-
 });
 
 
@@ -122,6 +139,9 @@ let gold999DataDots = [];
 for (var i = 0, l = 12; i < l; i++) {
    gold999DataDots.push(Math.round(Math.random() * (4100 - 3500) + 3500))
 };
+
+// document.querySelectorAll('.graphics__top-price')[1].firstChild.innerHTML = gold999DataDots[11];
+
 
 let gold999Data = {
    labels: ["01.01", "01.02", "01.03", "01.04", "01.05", "01.06", "01.07", "01.08", "01.09", "01.10", "01.11", "01.12"],
@@ -151,7 +171,9 @@ let silverDataDots = [];
 for (var i = 0, l = 12; i < l; i++) {
    silverDataDots.push(Math.round(Math.random() * (70 - 20) + 20))
 };
+// document.querySelectorAll('.graphics__top-price')[2].firstChild.innerHTML = silverDataDots[11];
 
+// document.querySelectorAll('.graphics__top-price p:nth-child(2)').innerHTML = goldData[11];
 let silverData = {
    labels: ["01.01", "01.02", "01.03", "01.04", "01.05", "01.06", "01.07", "01.08", "01.09", "01.10", "01.11", "01.12"],
    datasets: [{
@@ -175,3 +197,128 @@ let lineChartSilver = new Chart(silverCanvas, {
 });
 
 
+/*SELLSELLSELLSELLSELLSELLSELLSELLSELLSELLSELLSELLSELLSELL*/
+// var table = document.getElementById("tableId");
+// var rows = table.getElementsByTagName("tr");
+
+
+// rows.forEach(function (item) {
+//    item.addEventListener("click", function () {
+//       console.log('sdfsdf')
+//    }
+//    );
+// });
+
+// function showSell() {
+//    let typeLeft = document.querySelector('.sell__form-type')
+//    let typeRight = document.querySelectorAll('.sell__right-type')
+//    typeLeft.value = typeRight.innerHTML
+// }
+
+// function rowClick() {
+//    const table = document.getElementById('table');
+//    const typeLeft = document.querySelector('.sell__form-type')
+//    const tableRow = document.querySelectorAll('.sell__right-tablerow')
+
+
+//    for (let i = 1; i < tableRow.length; i++) {
+//       tableRow[i] = function () {
+//          document.querySelector('.sell__form-type').value
+//       }
+//    }
+// }
+
+let rows = document.getElementsByClassName("sell__right-tablerow")
+if (rows) {
+   for (var i = 1, len = rows.length; i < len; i++) {
+
+      rows[i].addEventListener('click', set_handler(i));
+   }
+
+   function set_handler(i) {
+      return function (e) {
+         const cell = rows[i].getElementsByTagName("td")[0].innerHTML
+         document.getElementById("sellType").value = cell;
+         document.getElementsByClassName('sell__left-item')[1].classList.remove('active');
+
+         document.getElementsByClassName('sell__left-item')[0].classList.add('active');
+      };
+   }
+}
+
+let silverRows = document.getElementsByClassName("silver__right-tablerow")
+if (silverRows) {
+   for (var i = 1, len = silverRows.length; i < len; i++) {
+
+      silverRows[i].addEventListener('click', set_handler(i));
+   }
+
+   function set_handler(i) {
+      return function (e) {
+         const cell = silverRows[i].getElementsByTagName("td")[0].innerHTML
+         document.getElementById("sellType").value = cell;
+         document.getElementsByClassName('sell__left-item')[0].classList.remove('active');
+
+         document.getElementsByClassName('sell__left-item')[1].classList.add('active');
+      };
+   }
+}
+
+// function addRowHandlers(e) {
+
+
+//    var cell = document.getElementsByTagName("td")[0];
+//    console.log(cell);
+//    var id = cell.innerHTML;
+//    console.log(document.getElementById("sellType"));
+//    document.getElementById("sellType").value = id;
+//    // document.getElementsByClassName('sell__left-item')[1].classList.remove('active');
+
+//    document.getElementsByClassName('sell__left-item')[0].classList.add('active');
+
+
+// }
+
+// function addSilverRowHandlers() {
+//    var table = document.getElementById("silverTable");
+//    var rows = table.getElementsByTagName("tr");
+
+
+//    for (i = 1; i < rows.length; i++) {
+//       var currentRow = table.rows[i];
+//       var createClickHandler =
+//          function (row) {
+//             return function () {
+//                var cell = row.getElementsByTagName("td")[0];
+//                var id = cell.innerHTML;
+//                document.getElementById("sellType").value = id;
+//                document.getElementsByClassName('sell__left-item')[0].classList.remove('active');
+
+//                document.getElementsByClassName('sell__left-item')[1].classList.add('active');
+
+//             };
+//          };
+
+//       currentRow.onclick = createClickHandler(currentRow);
+//    }
+// }
+
+setTimeout(function () {
+
+   odometer1.innerHTML = goldData[11];
+   odometer2.innerHTML = gold999DataDots[11];
+   odometer3.innerHTML = silverDataDots[11];
+   //document.querySelectorAll('.graphics__top-price')[0].firstChild.innerHTML = goldData[11];
+
+}, 100);
+
+window.odometerOptions = {
+   format: 'd',
+}
+
+
+
+const myModal = new HystModal({
+   linkAttributeName: "data-hystmodal",
+   //настройки, см. API
+});
